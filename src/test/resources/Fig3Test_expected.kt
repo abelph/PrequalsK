86f133a992b32b55bb665fa26cf96efd377fcc26
David Gipson <david.gipson@utexas.edu>
2021-11-04 15:55:40 -0500

    Correct first test

--- src/test/resources/Fig3Test_expected.kt
+++ src/test/resources/Fig3Test_expected.kt
@@ -1,8 +1,8 @@
-import XCTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Fig3Test: XCTestCase {
 
-    @Test
     fun testFig3Value0() {
         val actual = C(0).x
         XCTAssertEqual(actual, 0)
