b6d5e86605851c767d40aa24e7c9b3ed1f30106f
abelph <abelp76@gmail.com>
2021-11-10 22:50:32 -0600

    Added fig4 test

---
+++ src/test/resources/Fig4Test.kt
@@ -0,0 +1,24 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Fig4(var s: String) {
+    fun greet(@argLabel(” ”) person: String, @argLabel(”from”) hometown: String): String {
+        s = ”Hello ${person}!\n”
+        s += ”Glad you could visit from ${hometown}.”
+        return s
+    }
+    fun main() {
+        greet(”Bill”, ”Cupertino”)
+    }
+}
+
+class Fig4Test {
+
+    @Test
+    fun testFig4() {
+        val f = Fig4("")
+        val expected = "Hello Bill!\nGlad you could visit from Cupertino."
+        f.greet("Bill", "Cupertino")
+        assertEquals(expected, f.s)
+    }
+}
