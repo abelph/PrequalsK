f8f44d7c206cff42571df24bb5831b99d62ad671
David Gipson <david.gipson@utexas.edu>
2021-11-13 19:38:25 -0600

    Add more test methods to Fig3Test

--- src/test/resources/Fig3Test.kt
+++ src/test/resources/Fig3Test.kt
@@ -12,4 +12,16 @@ class Fig3Test {
         assertEquals(0, actual)
     }
 
+    @Test
+    fun testFig3ValueNegative() {
+        val actual = C(-1).x
+        assertEquals(-1, actual)
+    }
+
+    @Test
+    fun testFig3ValueMax() {
+        val actual = C(2_147_483_647).x
+        assertEquals(2_147_483_647, actual)
+    }
+
 }
--- src/test/resources/Fig3Test_expected.kt
+++ src/test/resources/Fig3Test_expected.kt
@@ -11,4 +11,14 @@ val actual = C(0).x
 XCTAssertEqual(actual, 0)
 }
 
+ fun testFig3ValueNegative(){
+  val actual = C(-1).x
+  XCTAssertEqual(actual, -1)
+ }
+
+ fun testFig3ValueMax(){
+  val actual = C(2_147_483_647).x
+  XCTAssertEqual(actual, 2_147_483_647)
+ }
+
 }
